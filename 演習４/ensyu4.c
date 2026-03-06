1cad75afbd6af60576634b6d440c6679144b7a8d
DESKTOP-EKEPCKU\UCRE <kusakabe@ace-software.com>
2026-03-06 15:26:40 +0900

    修正

--- 演習４/ensyu4.c
+++ 演習４/ensyu4.c
@@ -2,17 +2,17 @@
 
 int main(void){
     int count = 0;
-    float num = 0, total = 0, ave = 0;
+    float input_num = 0, total = 0, ave = 0;
     printf("数値を入力 0で終了\n");
-    scanf("%d", &num);
-    if(num == 0){
+    scanf("%d", &input_num);
+    if(input_num == 0){
         printf("終了\n");
         return 0;
     }
 
     while (1)
     {
-        if(num != 0){
+        if(input_num != 0){
             total += num;
             count++;
         }else if (num == 0){
--- 演習６/ensyu6.c
+++ 演習６/ensyu6.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-int main(void){
-    int n = 17, i;
-    int f1 = 0, f2 = 1, next = 0;
-    for(i = 0; i < n; i++){
-       printf("%d", f1);
+#define MAX 1000
 
-       if(i < n){
-        printf(",");
-       }
-       
-       next = f1 + f2;
-       f1 = f2;
-       f2 = next;
+int main(void){
+    int f1 = 0, f2 = 1, next = 0;
+
+    while(1){
+        if(f1 < MAX){
+            printf("%d", f1);
+            if(f2 < MAX){
+                printf(",");
+            }
+        }else{
+            break;
+        }
+
+        
+
+        next = f1 + f2;
+        f1 = f2;
+        f2 = next;
     }
+
     printf("\n");
     return 0;
 }
